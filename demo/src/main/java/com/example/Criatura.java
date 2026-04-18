d7bf7af0e7deee5ce14aa92f66a1fc3fa4227f2b
John Steban Morales Ceron <jstebanmorales@estudiante.uniajc.edu.co>
2026-04-17 20:58:26 -0500

    Agregando clase criatura

---
+++ demo/src/main/java/com/example/Criatura.java
@@ -0,0 +1,37 @@
+package com.example;
+
+public abstract class Criatura {
+    protected String nombre;
+    protected int salud;
+    protected int fuerza;
+    protected Arma arma;
+
+    public Criatura(String nombre, int salud, int fuerza) {
+        this.nombre = nombre;
+        this.salud = salud;
+        this.fuerza = fuerza;
+    }
+
+    public abstract void atacar(Criatura objetivo);
+    public abstract void defender(int daño);
+
+    public boolean estaViva() {
+        return salud > 0;
+    }
+
+    public void equiparArma(Arma arma) {
+        this.arma = arma;
+    }
+
+    public void desequiparArma() {
+        this.arma = null;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getSalud() {
+        return salud;
+    }
+}
